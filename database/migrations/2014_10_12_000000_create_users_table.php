4276db242faa628eb88f28d54f23a28305ad5934
= <=>
2022-06-05 17:17:46 +0200

    fix migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 class CreateUsersTable extends Migration
@@ -24,6 +26,46 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
+
+        User::create([
+            'name' => 'Kiriam',
+            'email' => 'kiriamcf@gmail.com',
+            'password' => Hash::make('kiriam-cf-21-12-2001'),
+            'admin' => 1,
+            'targeta' => '86 77 9C 14'
+        ]);
+
+        User::create([
+            'name' => 'Roger',
+            'email' => 'roger.escudero@estudiantat.upc.edu',
+            'password' => Hash::make('Rogerescudero5'),
+            'admin' => 1,
+            'targeta' => 'DD 9E C0 F6'
+        ]);
+
+        User::create([
+            'name' => 'Elias',
+            'email' => 'elias.ben-seddik@estudiantat.upc.edu',
+            'password' => Hash::make('elmascabrondetantos'),
+            'admin' => 1,
+            'targeta' => '46 66 A9 14'
+        ]);
+
+        User::create([
+            'name' => 'Christian',
+            'email' => 'christian.mambo-matala@estudiantat.upc.edu',
+            'password' => Hash::make('ChristiaN15'),
+            'admin' => 1,
+            'targeta' => '3D 90 BF F6'
+        ]);
+
+        User::create([
+            'name' => 'Normal_User',
+            'email' => 'NormalUser@gmail.com',
+            'password' => Hash::make('normaluser'),
+            'admin' => 1,
+            'targeta' => 'AB CD EF GH'
+        ]);
     }
 
     /**
--- database/migrations/2022_05_04_201458_create_machines_table.php
+++ database/migrations/2022_05_04_201458_create_machines_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Machine;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -19,6 +20,36 @@ class CreateMachinesTable extends Migration
             $table->text('description');
             $table->timestamps();
         });
+
+        Machine::create([
+            'name' => 'Maquina 3D',
+            'description' => "Màquina que serveix per produir representacions 3D físiques de models creats per ordinador, mitjançant una tècnica anomenada fabricació additiva."
+        ]);
+
+        Machine::create([
+            'name' => 'Torn',
+            'description' => "És un tipus de màquina-eina que permet mecanitzar peces de forma geomètrica de revolució. Es pot accionar a través d'una maneta o d'un motor."
+        ]);
+
+        Machine::create([
+            'name' => 'Fresadora',
+            'description' => "És una màquina eina utilitzada per donar formes complexes a peces de metall, plàstic, fusta o altres materials. Fa operacions com tall de ranures, planejats, perforacions..."
+        ]);
+
+        Machine::create([
+            'name' => 'Maquina de cosir',
+            'description' => "És una màquina que s'utilitza per cosir tela i altres materials juntament amb fil. Porta un protector d'agulles, per tal d'evitar lesions accidentals per punxades."
+        ]);
+
+        Machine::create([
+            'name' => 'Talladora laser',
+            'description' => "Màquina que fa servir el tallatge o tallament làser, que és una tecnologia que fa ús d'un raig làser per tallar diferents materials amb molt bona precisió."
+        ]);
+
+        Machine::create([
+            'name' => 'Trepant',
+            'description' => "Màquina o eina amb la que es mecanitzen la majoria de forats que es fan a les peces dels tallers mecànics. Es caracteritza per ser molt fàcil de fer servir."
+        ]);
     }
 
     /**
